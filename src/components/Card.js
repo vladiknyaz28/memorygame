5b6b15e80998a5315687045369ff2f7007832f52
Vlad Knyazev <vladiknyaz28@gmail.com>
2026-06-27 15:09:34 +0300

    fix card image paths

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,33 +3,31 @@ import './Card.css';
 
 class Card extends React.Component {
 
-
-
   cardClickHandler = (item) => {
     this.props.onChoice(item);
   }
 
   render() {
     return (
-      <div className={'card ' + (this.props.isShowed ? 'opened' : 'closed')}
-           onClick={this.cardClickHandler.bind(this, this.props.item)}>
+      <div
+        className={'card ' + (this.props.isShowed ? 'opened' : 'closed')}
+        onClick={this.cardClickHandler.bind(this, this.props.item)}
+      >
         <div className="card-inner card-front">
           <img
-            src={'/images/' + this.props.item.image}
+            src={this.props.item.image}
             alt={this.props.item.name}
           />
         </div>
+
         <div className="card-inner card-back">
           <img
-            src="/images/question.svg"
+            src={process.env.PUBLIC_URL + '/images/question.svg'}
             alt="Question mark"
           />
         </div>
-
-
       </div>
-      )
-
+    );
   }
 }
 
